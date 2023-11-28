2193c291e3d45f6be8af2391b3d92c27cdc2240a
Marwane <khmarwane10@gmail.com>
2023-11-28 09:15:44 -0800

    Regular expression

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,14 +1,3 @@
 #!/usr/bin/env ruby
-
-# Get the first command-line argument
-input_arg = ARGV[0]
-
-# Define the regex pattern
-pattern = /hbt{2,5}n/
-
-# Use the scan method to find all occurrences of the pattern
-matches = input_arg.scan(pattern)
-
-# Print the result
-puts matches.join
+puts ARVG[0].scan(/hbt{2,5}n/).join
 ~
