a308677b8c18e73eb0e6af5c1de2207f11cdb49c
Marwane <khmarwane10@gmail.com>
2023-11-28 09:18:07 -0800

    Regular expression

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,3 +1,13 @@
 #!/usr/bin/env ruby
-puts ARVG[0].scan(/hbt{2,5}n/).join
-~
+
+# Get the first command-line argument
+input_argument = ARGV[0]
+
+# Define the regex pattern
+pattern = /hbt{2,5}n/
+
+# Use the scan method to find all occurrences of the pattern
+matches = input_argument.scan(pattern)
+
+# Print the result
+puts matches.join
