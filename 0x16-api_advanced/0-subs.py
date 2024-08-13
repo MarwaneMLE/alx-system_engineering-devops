d33730cbcc5a9925023e553cd6a30aa1cbe8ef38
MarwaneMLE <khmarwane10@gmail.com>
2024-08-13 08:51:48 -0700

    Get number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,10 +15,10 @@ def number_of_subscribers(subreddit):
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code != 200:
-        return (0)
+        return 0
     response = response.json()
     if 'data' in response:
         return (response.get('data').get('subscribers'))
 
     else:
-        return (0)
+        return 0
